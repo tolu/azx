9497b160e0acda77f9b8c97752551deea36b63e5
Tobias Lundin <tobias.lundin@gmail.com>
2018-03-22 12:51:01 +0100

    initial implementation and readme

---
+++ az.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const {spawn} = require('child_process');
+
+const cmd = path.join('c:', 'Program Files (x86)', 'Microsoft SDKs', 'Azure', 'CLI2', 'wbin', 'az.cmd');
+
+const setSubscription = async subscription => {
+  await execFn(['account', 'set', '-s', subscription.id]);
+  console.log('Subscription set to', subscription.id);
+};
+
+const getSubscriptions = async () => {
+  const stdout = await execFn(['account', 'list']);
+  const json = JSON.parse(stdout);
+  return json.map(s => ({
+    name: `${s.name} (${s.user.name})`,
+    value: s.id,
+    id: s.id,
+    isDefault: s.isDefault
+  }));
+};
+
+/**
+ * @returns {Promise<{name: string, value: string}[]>}
+ */
+const getContainerRegistries = async () => {
+  const stdout = await execFn(['acr', 'list']);
+  const json = JSON.parse(stdout);
+  return json.map(s => ({
+    name: s.name,
+    value: s.resourceGroup
+  }));
+};
+
+/**
+ * @param {string} acrName
+ * @returns {Promise<string[]>}
+ */
+const getRegistryRepositories = async acrName => {
+  const stdout = await execFn(['acr', 'repository', 'list', '-n', acrName]);
+  return JSON.parse(stdout);
+};
+
+/**
+ * @param {string} acrName
+ * @param {string} repository
+ * @returns {Promise<string[]>}
+ */
+const getRepositoryTags = async (acrName, repository) => {
+  const stdout = await execFn(['acr', 'repository', 'show-tags', '-n', acrName, '--repository', repository]);
+  return JSON.parse(stdout);
+};
+
+function execFn(args) {
+  let response = '';
+  return new Promise((resolve, reject) => {
+    // Console.log(`Spawn: "az ${args.join(' ')}"`)
+    const p = spawn(cmd, args);
+    p.stderr.on('data', reject);
+    p.stdout.on('data', data => {
+      response += data;
+    });
+    p.on('close', () => resolve(response.trim()));
+  });
+}
+
+module.exports = {
+  getSubscriptions,
+  setSubscription,
+  getContainerRegistries,
+  getRegistryRepositories,
+  getRepositoryTags
+};
---
+++ index.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const {prompt} = require('inquirer');
+const {desc: sortTags} = require('semver-sort');
+const {
+  showLoader,
+  hideLoader
+} = require('./loading');
+const {
+  getSubscriptions,
+  setSubscription,
+  getContainerRegistries,
+  getRegistryRepositories,
+  getRepositoryTags
+} = require('./az');
+// @ts-ignore
+const version = require('./package.json').version;
+
+program.name('azx');
+
+program
+  .version(version, '-v, --version')
+  .usage('[command] [options]')
+  .option('-d, --debug', 'verbose output', false)
+  .description('Interactive <az> helper');
+
+program
+  .command('set-subscription')
+  .description('select az subscription')
+  .alias('s')
+  .action(async () => {
+    showLoader('Fetching subscriptions...');
+    const subscriptions = await getSubscriptions();
+    hideLoader();
+    prompt([{
+      type: 'list',
+      name: 'id',
+      message: `Select az subscription`,
+      choices: subscriptions,
+      default: subscriptions.indexOf(subscriptions.find(s => s.isDefault))
+    }])
+      .then(({id}) => setSubscription(subscriptions.find(s => s.id === id)));
+  });
+
+program
+  .command('get-tags')
+  .description('list tags for image in acr')
+  .alias('gt')
+  .action(async () => {
+    const state = {
+      registry: null,
+      repository: null
+    };
+    showLoader('Fetching acr registries...');
+    const registries = await getContainerRegistries();
+    hideLoader();
+    // Auto-select if only one available
+    if (registries.length > 1) {
+      const {value} = await prompt([{
+        type: 'list',
+        name: 'value',
+        message: `Select az registry`,
+        choices: registries
+      }]);
+      state.registry = registries.find(r => r.value === value);
+    } else {
+      console.log('Auto selected registry...');
+      state.registry = registries[0];
+    }
+    showLoader('Fetching acr repositories...');
+    const repositories = await getRegistryRepositories(state.registry.name);
+    hideLoader();
+    const {value} = await prompt([{
+      type: 'list',
+      name: 'value',
+      message: `Select az registry`,
+      choices: repositories
+    }]);
+    state.repository = value;
+
+    showLoader('Fetching tags...');
+    const tags = await getRepositoryTags(state.registry.name, state.repository);
+    hideLoader();
+    console.log(sortTags(tags));
+  });
+
+program.parse(process.argv);
+
+// Default to showing help
+if (program.args.length === 0) {
+  program.help();
+}
